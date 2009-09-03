6e2855081334ae3c41451653470aa39462feed7a
Sulik Szabolcs <sulik.szabolcs@gmail.com>
2009-09-03 09:11:23 +0200

    2009 qr solution to problem a

---
+++ 2009_0903_qr/solution_a.py
@@ -0,0 +1,34 @@
+import sys
+import re
+
+def fgets(f):
+  return f.readline().rstrip("\n")
+
+
+def solve_case(p, words):
+  pattern = p.replace('(', '[').replace(')', ']')
+  
+  count = 0
+  for word in words:
+    if re.match(pattern, word):
+      count += 1
+  return count
+
+if __name__ == '__main__':
+  fin = open(sys.argv[1], 'r')
+  fout = open(sys.argv[1]+'.out', 'w')
+  
+  L, D, N = map(int, re.findall('\d+', fgets(fin)))
+  
+  words = []
+  for i in range(D):
+    words.append(fgets(fin))
+  
+  for case_index in range(N):
+    m = solve_case(fgets(fin), words)
+    r = "Case #%d: %d" % (case_index + 1, m)
+    print r
+    fout.write(r + "\n")
+    
+  fin.close()
+  fout.close()
