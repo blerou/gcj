3035dbecac757517904378cfc4b240497f49477b
blerou <sulik.szabolcs@gmail.com>
2015-04-11 20:11:18 +0200

    2015 QR - B problem

---
+++ 2015_qr/b.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def read_input(s):
+    data = filter(len, s.replace("\n", " ").split(" "))
+    data = map(int, data)
+    result = []
+    for _ in range(data.pop(0)):
+        d = data.pop(0)
+        ps = dict()
+        for p in data[:d]:
+            if p not in ps:
+                ps[p] = 0
+            ps[p] += 1
+        data = data[d:]
+        result.append(ps)
+    return result
+
+
+def solve(ps):
+    mins = 0
+    sm = 1001
+    while True:
+        # print ps, "@", mins
+        pmax = max(ps.keys())
+        sm = min(sm, pmax + mins)
+        pcnt = ps[pmax]
+        small_half = pmax / 2
+        large_half = pmax - small_half
+        if pcnt <= small_half:
+            if small_half not in ps:
+                ps[small_half] = 0
+            if large_half not in ps:
+                ps[large_half] = 0
+            ps[small_half] += pcnt
+            ps[large_half] += pcnt
+            del ps[pmax]
+            mins += pcnt
+        else:
+            return sm
+
+
+if __name__ == "__main__":
+    cases = read_input(sys.stdin.read())
+    for i in range(len(cases)):
+        print "Case #%d: %s" % (i + 1, solve(cases[i]))
